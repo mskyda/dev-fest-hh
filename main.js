8f26c463d2d740cc6cd72e4ccefb03ea8196bac4
Misha Skyda <mihail.skida@gmail.com>
2017-11-26 12:53:56 +0100

    run voice recognition

--- main.js
+++ main.js
@@ -43,7 +43,55 @@ App.prototype = {
 
 			console.log('Received token: ' + token);
 
-		});
+			this.token = token;
+
+			this.listenTopic();
+
+			this.startRecognition();
+
+		}.bind(this));
+
+	},
+
+	listenTopic: function(){
+
+		var fetchUrl = 'https://iid.googleapis.com/iid/v1/' + this.token + '/rel/topics/' + this.topic;
+
+		fetch(fetchUrl, {
+			'method': 'POST',
+			'headers': {
+				'Authorization': 'key=' + this.appKey,
+				'Content-Type': 'application/json'
+			}
+		}).then(function() {
+			console.log('Listening topic: "' + this.topic + '"');
+		}.bind(this));
+
+	},
+
+	startRecognition: function(){
+
+		this.recognition.lang = 'en-EN';
+		this.recognition.continuous = false;
+		this.recognition.interimResults = false;
+		this.recognition.addEventListener('result', this.onRecognitionResult);
+		this.recognition.addEventListener('end', this.recognition.start);
+
+		this.recognition.start();
+
+	},
+
+	onRecognitionResult: function(event){
+
+		for (var i = event.resultIndex; i < event.results.length; i++) {
+
+			if(event.results[i].isFinal){
+
+				console.log(event.results[i][0].transcript);
+
+			}
+
+		}
 
 	}
 
